016c1110324be464d097a699c1a02e48b58efee8
IgorVaschenko <103171728+IgorVaschenko@users.noreply.github.com>
2022-09-11 20:23:22 +0300

    rollup plugins html

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from '@/components/App/App';
 import toast from "@/containers/ToastCore/ToastClass";
 import ToastContainer from "@/components/Toast/ToastContainer";
 
+let root = document.createElement('div');
+root.id = "root";
+document.body.prepend(root);
+
 ReactDOM.render(<App />, document.querySelector('#root'));
 
 export {
